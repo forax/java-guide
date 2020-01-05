83a236d8bbb87229fc54aafa01c928279726b463
forax <forax@univ-mlv.fr>
2020-01-05 22:26:48 +0100

    refactor to introduce jupyter generation

--- build/build.java
+++ build/build.java
@@ -1,6 +1,7 @@
 import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.readAllLines;
 import static java.nio.file.Files.writeString;
+import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,7 +12,7 @@ import java.util.function.Predicate;
 
 @SuppressWarnings("preview")
 class build {
-  static class Text {
+  static class TextBuilder {
     private final StringBuilder builder = new StringBuilder();
     
     void append(String line) {
@@ -30,7 +31,7 @@ class build {
     CODE
     ;
     
-    String transform(String line) {
+    String clean(String line) {
       return switch(this) {
       case BLANK, CODE -> line;
       case TEXT -> line.substring(3); 
@@ -48,47 +49,94 @@ class build {
     }
   }
   
-  private static String transformToMarkDown(List<String> lines) {
-    var text = new Text();
+  interface EventHandler {
+    default void startDocument(@SuppressWarnings("unused") TextBuilder builder) { /*empty*/ }
+    default void endDocument(@SuppressWarnings("unused") TextBuilder builder) { /*empty*/ }
+    default void startCode(@SuppressWarnings("unused") TextBuilder builder) { /*empty*/ }
+    default void endCode(@SuppressWarnings("unused") TextBuilder builder) { /*empty*/ }
+    default void startText(@SuppressWarnings("unused") TextBuilder builder) { /*empty*/ }
+    default void endText(@SuppressWarnings("unused") TextBuilder builder) { /*empty*/ }
+    void line(TextBuilder builder, LineKind kind, String line);
+  }
+  
+  private static String transformTo(List<String> lines, EventHandler handler) {
+    var builder = new TextBuilder();
+    handler.startDocument(builder);
     
-    var insideCode = false;
+    var inside = LineKind.BLANK;
     var skipFirst = true;
     for(var line: lines) {
       var kind = LineKind.kind(line);   
       
       //System.out.println(kind + " " + line);
       
-      insideCode = switch(kind) {
-        case BLANK, TEXT -> {
-          if (insideCode) {
-            text.append("```");
+      inside = switch(kind) {
+        case BLANK -> {
+          if (inside == LineKind.CODE) {
+            handler.endCode(builder);
           }
-          yield false;
+          yield (inside == LineKind.TEXT)? LineKind.TEXT: LineKind.BLANK;
+        }
+        case TEXT -> {
+          if (inside == LineKind.CODE) {
+            handler.endCode(builder);
+          }
+          if (inside != LineKind.TEXT) {
+            handler.startText(builder);
+          }
+          yield kind;
         }
         case CODE -> {
-          if (!insideCode) {
-            text.append("```java");
+          if (inside == LineKind.TEXT) {
+            handler.endText(builder);
           }
-          yield true;
+          if (inside != LineKind.CODE) {
+            handler.startCode(builder);
+          }
+          yield kind;
         }
       };
       
       skipFirst = skipFirst && kind == LineKind.TEXT;
       if (!skipFirst) {
-        text.append(kind.transform(line));
+        handler.line(builder, kind, kind.clean(line));
       }
     }
     
-    if (insideCode) {
-      text.append("```");
+    if (inside == LineKind.CODE) {
+      handler.endCode(builder);
+    } else if (inside == LineKind.TEXT) {
+      handler.endText(builder);
     }
-    return text.toString();
+    handler.endDocument(builder);
+    return builder.toString();
   }
   
-  private static void convertToMarkDown(Path from, Path to) throws IOException {
-    createDirectories(to.getParent());
-    writeString(to, transformToMarkDown(readAllLines(from)));
+  private static void writeMarkDown(List<String> lines, Path to) throws IOException {
+    writeString(to, transformTo(lines, new EventHandler() {
+      @Override
+      public void startCode(TextBuilder text) {
+        text.append("```java");
+      }
+      @Override
+      public void endCode(TextBuilder text) {
+        text.append("```");
+      }
+      @Override
+      public void line(TextBuilder text, LineKind kind, String line) {
+        text.append(line);
+      }
+    }));
   }
+  
+  /*private static void writeJupyter(List<String> lines, Path to) throws IOException {
+    writeString(to, transformTo(lines, new EventHandler() {
+      @Override
+      public void line(TextBuilder text, LineKind kind, String line) {
+        
+      }
+    }));
+  }*/
   
   private static String removeExtension(String filename) {
     var index = filename.lastIndexOf('.');
@@ -106,21 +154,40 @@ class build {
     return filename.substring(index + 1);
   }
   
-  private static void generateMarkDown(Path directory, Predicate<String> filter, Path destination) throws IOException {
+  private static void generate(List<Path> paths, Path markdownFolder, Path jupyterFolder) throws IOException {
+    createDirectories(markdownFolder);
+    createDirectories(jupyterFolder);
+    
+    for(var path: paths) {
+      var rawFilename = removeExtension(path.getFileName().toString());
+
+      var lines = readAllLines(path);
+      writeMarkDown(lines, markdownFolder.resolve(rawFilename + ".md"));
+      //writeJupyter(lines, jupyterFolder.resolve(rawFilename + ".ipynb"));
+    }
+  }
+  
+  private static void generateIndex(List<Path> paths, Path destination) {
     var index = 0;
+    for(var path: paths) {
+      var filename = removeExtension(path.getFileName().toString()) + ".md";
+      System.out.println(index + ". [" + shortName(filename) + "](" + destination.getFileName() + "/" + filename + ")");
+      index++;
+    }
+  }
+  
+  private static List<Path> gatherFiles(Path directory, Predicate<String> filter) throws IOException {
     try(var list = Files.list(directory)) {
-      for(var from: (Iterable<Path>)list.filter(p -> filter.test(p.toString())).sorted(Comparator.comparing(Path::toString))::iterator) {
-        var filename = removeExtension(from.getFileName().toString()) + ".md";
-        var to = destination.resolve(filename);
-        
-        System.out.println(index + ". [" + shortName(filename) + "](" + destination.getFileName() + "/" + filename + ")");
-        convertToMarkDown(from, to);
-        index++;
-      }
+      return list.filter(path -> filter.test(path.toString())).sorted(Comparator.comparing(Path::toString)).collect(toList());
     }
   }
   
   public static void main(String[] args) throws IOException {
-    generateMarkDown(Path.of("."), name -> name.endsWith(".jsh"), Path.of("guide"));
+    var markdownFolder = Path.of("guide");
+    var jupyterFolder = Path.of("jupyter");
+    
+    var files = gatherFiles(Path.of("."), name -> name.endsWith(".jsh"));
+    generate(files, markdownFolder, jupyterFolder);
+    generateIndex(files, markdownFolder);
   }
-} 
+}
